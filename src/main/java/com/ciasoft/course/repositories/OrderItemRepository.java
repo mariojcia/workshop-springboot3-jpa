76ccb227a718b37dcdb66a0d22f3c77d8b8a80a9
Mario Cia <marioj.cia@gmail.com>
2024-07-09 16:55:32 -0300

    OrderItem, many-to-many association with extra attributes PART 2

---
+++ src/main/java/com/ciasoft/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.ciasoft.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ciasoft.course.entities.OrderItem;
+
+public interface OrderItemRepository  extends JpaRepository<OrderItem, Long>{
+
+}
